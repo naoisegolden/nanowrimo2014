a9dbafa370eaf88c4a2d622f5cb323263728f812
Naoise Golden <naoise.golden@xing.com>
2014-11-01 12:55:58 +0100

    Añadir contar palabras fichero en script

--- count.sh
+++ count.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
-
 for f in *-count.md; do
     words=( $(< "$f") )
     (( num += ${#words[@]} ))
 done
-echo Word count: $num.
+echo Total word count: $num.
+
+for f in $1; do
+    words=( $(< "$f") )
+    (( num2 += ${#words[@]} ))
+done
+echo File word count: $num2
